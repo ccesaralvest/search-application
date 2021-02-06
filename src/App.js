246d8886c0792ca027a1d0691d0ff264513ff233
Caio Cesar Alves Timoteo <caio.timoteo@performait.com>
2021-02-06 13:02:56 -0300

    Search Application Init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Search Application
         </a>
       </header>
     </div>
